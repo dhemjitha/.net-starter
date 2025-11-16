490577eca9fe621958c873d598590f72d7c4f05e
Dulran Hemjitha <dhemjitha@gmail.com>
2025-11-16 10:37:57 +0530

    feat: configure CORS for local React/Vite development

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// Configure CORS to allow calls from local React/Vite dev servers
+var reactCorsPolicy = "_reactDevCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: reactCorsPolicy, policy =>
+    {
+        policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,6 +27,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enable CORS for browser-based frontends (React/Vite)
+app.UseCors(reactCorsPolicy);
 
 app.UseAuthorization();
 
